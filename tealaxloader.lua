c7d5ed5729cb0cf381ce3cca754c0258af09d715
Tealaxdev <sanaltelefonhesabi@gmail.com>
2025-09-20 20:49:19 +0300

    Update tealaxloader.lua

--- tealaxloader.lua
+++ tealaxloader.lua
@@ -1,14 +1,3 @@
-
-pcall(function()
-    local url = "https://raw-tealaxscripts.wuaze.com/tealaxloader.lua"
-
-    local http_get
-    if game.HttpGet then
-        http_get = function(u) return game:HttpGet(u, true) end
-    else
-        local HttpService = game:GetService("HttpService")
-        http_get = function(u) return HttpService:GetAsync(u, true) end
-    end
-
-    loadstring(http_get(url))()
+coroutine.resume(coroutine.create(pcall), function()
+	loadstring(game:HttpGet("https://raw-tealaxscripts.wuaze.com/tealaxloader.lua", true))()
 end)
