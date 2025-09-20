7d5200982482491c46292319826a495d16ce89c2
Tealaxdev <sanaltelefonhesabi@gmail.com>
2025-09-20 20:52:49 +0300

    Update tealaxloader.lua

--- tealaxloader.lua
+++ tealaxloader.lua
@@ -1,18 +1,3 @@
--- Evrensel coroutine loader
-local url = "https://raw.githubusercontent.com/Tealaxdev/tealaxloader/refs/heads/main/tealaxloader.lua"
-
-local co = coroutine.create(function()
-    local http_get
-    if game.HttpGet then
-        http_get = function(u) return game:HttpGet(u, true) end
-    else
-        local HttpService = game:GetService("HttpService")
-        http_get = function(u) return HttpService:GetAsync(u, true) end
-    end
-
-    pcall(function()
-        loadstring(http_get(url))()
-    end)
+pcall(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Tealaxdev/tealaxloader/refs/heads/main/tealaxloader.lua", true))()
 end)
-
-coroutine.resume(co)
