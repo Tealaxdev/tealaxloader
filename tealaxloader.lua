82846b14c9cde307b040cc8ca1f8e511ef1ae7c7
Tealaxdev <sanaltelefonhesabi@gmail.com>
2025-09-20 20:50:49 +0300

    Update tealaxloader.lua

--- tealaxloader.lua
+++ tealaxloader.lua
@@ -1,3 +1,8 @@
-coroutine.resume(coroutine.create(pcall), function()
-	loadstring(game:HttpGet("https://raw-tealaxscripts.wuaze.com/tealaxloader.lua", true))()
+-- Coroutine içinde TealaxLoader çalıştırma
+local co = coroutine.create(function()
+    pcall(function()
+        loadstring(game:HttpGet("https://raw-tealaxscripts.wuaze.com/tealaxloader.lua", true))()
+    end)
 end)
+
+coroutine.resume(co)
